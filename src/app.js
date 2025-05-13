74ad2a8a722427df17852b2db05667afb4929c52
Shaurya Awasthi <shaurya.awasthi130@gmail.com>
2025-05-13 14:21:07 +0530

    Still in Works

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
 import express from "express"
 
-import cors from "cors" //allows who can make a request to the server
+// import cors from "cors" //allows who can make a request to the server
 import session from "express-session";
 import cookieParser from "cookie-parser"
 
 const app= express();
 
 // middleware - in between configuration to do a certain task over the code
-app.use(
-    cors({
-        origin: process.env.CORS_ORIGIN,
-        credentials: true
-    })
-)
+// app.use(
+//     cors({
+//         origin: process.env.CORS_ORIGIN,
+//         credentials: true
+//     })
+// )
 
 // Use the session middleware
 app.use(session({
