76a6eb667cc3557c603433d3f4b3ee8a5cabe0b2
ziyamohammad <mohammad2311061@akgec.ac.in>
2025-08-17 03:52:56 +0530

    jb

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
 app.use(cors({
-    origin:process.env.CORS_ORIGIN,
+    origin:"https://genaievent.vercel.app",
     credentials:true
 }))
 // 2. Session Middleware
